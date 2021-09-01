e054139c6ddc9fcdc7feb98d3e47285639532b3b
fishedee <306766045@qq.com>
2021-09-01 15:25:36 +0800

    升级UtilBoost

--- server/src/main/java/com/fishedee/mymanager_ddd/framework/WebExceptionAdvice.java
+++ server/src/main/java/com/fishedee/mymanager_ddd/framework/WebExceptionAdvice.java
@@ -2,6 +2,7 @@ package com.fishedee.mymanager_ddd.framework;
 
 import com.fishedee.id_generator.IdGeneratorException;
 import com.fishedee.jpa_boost.JPABoostException;
+import com.fishedee.util_boost.web.UtilBoostExceptionAdvice;
 import com.fishedee.web_boost.LogTimeHandlerInterceptor;
 import com.fishedee.web_boost.WebBoostExceptionAdvice;
 import com.fishedee.web_boost.WebBoostResponseAdvice;
@@ -14,29 +15,5 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.validation.ValidationException;
 
 @ControllerAdvice
-public class WebExceptionAdvice extends WebBoostExceptionAdvice {
-    //自定义异常的拦截返回
-    @ExceptionHandler(JPABoostException.class)
-    @ResponseBody
-    public WebBoostResponseAdvice.ResponseResult exceptionHandler(JPABoostException e){
-        return new WebBoostResponseAdvice.ResponseResult(HttpStatus.OK,1,e.getMessage(),null);
-    }
-
-    @ExceptionHandler(IdGeneratorException.class)
-    @ResponseBody
-    public WebBoostResponseAdvice.ResponseResult exceptionHandler(IdGeneratorException e){
-        return new WebBoostResponseAdvice.ResponseResult(HttpStatus.OK,1,e.getMessage(),null);
-    }
-
-    @ExceptionHandler(ValidationException.class)
-    @ResponseBody
-    public WebBoostResponseAdvice.ResponseResult exceptionHandler(ValidationException e){
-        return new WebBoostResponseAdvice.ResponseResult(HttpStatus.OK,1,e.getMessage(),null);
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseBody
-    public WebBoostResponseAdvice.ResponseResult exceptionHandler(AccessDeniedException e){
-        return new WebBoostResponseAdvice.ResponseResult(HttpStatus.OK,1,"没有权限访问",null);
-    }
+public class WebExceptionAdvice extends UtilBoostExceptionAdvice {
 }
