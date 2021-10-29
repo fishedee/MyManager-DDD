8894cf4116071b3df82f234f8afaf3118fe035f2
fishedee <306766045@qq.com>
2021-10-29 19:41:41 +0800

    加入jdbcTemplate的实现

--- server/src/main/java/com/fishedee/mymanager_ddd/api/ConcurrentUserController.java
+++ server/src/main/java/com/fishedee/mymanager_ddd/api/ConcurrentUserController.java
@@ -3,11 +3,14 @@ package com.fishedee.mymanager_ddd.api;
 import com.fishedee.mymanager_ddd.business.ConcurrentUser;
 import com.fishedee.mymanager_ddd.infrastructure.ConcurrentUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/concurrentUser")
@@ -15,6 +18,9 @@ public class ConcurrentUserController {
 
     @Autowired
     private ConcurrentUserRepository concurrentUserRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
 
     //curl触发 curl http://localhost:9191/api/concurrentUser/incAge\?data\=%7B%22id%22%3A10001%7D
@@ -25,5 +31,13 @@ public class ConcurrentUserController {
     public void incAge(Long id){
         ConcurrentUser user = concurrentUserRepository.getForLock(id);
         user.incAge();
+    }
+
+    @GetMapping("/incAge2")
+    @Transactional
+    public void incAge2(Long id){
+        Map<String,Object> mapResult = this.jdbcTemplate.queryForMap("select * from concurrent_user where id = ? for update",id);
+        Integer oldAge = (Integer) mapResult.get("age");
+        this.jdbcTemplate.update("update concurrent_user set age = ? where id = ?",oldAge+1,id);
     }
 }
